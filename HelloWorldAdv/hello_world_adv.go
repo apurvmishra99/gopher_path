7e6ae09e0ec0e12d24455e8cae9884c8731e21b5
Apurv <apurvmishra99@gmail.com>
2020-05-01 22:56:13 +0530

    test passes

--- HelloWorldAdv/hello_world_adv.go
+++ HelloWorldAdv/hello_world_adv.go
@@ -10,6 +10,9 @@ const englishHelloPrefix = "Hello, "
 
 // Hello : An abstracted function, whichgo  is easier to test
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return englishHelloPrefix + name + "!"
 }
 
