9935f7e1c30f8f26bea2a3db6db9cda19bb89d8a
Apurv <apurvmishra99@gmail.com>
2020-05-04 19:32:51 +0530

    addded functionality

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,10 +1,10 @@
 package iteration
 
 // Repeat : returns a new string with the given string repeated 5 times.
-func Repeat(character string) string {
+func Repeat(character string, times int) string {
 	var rep string
-	for i := 0; i < 5; i++ {
-		rep = rep + character
+	for i := 0; i < times; i++ {
+		rep += character
 	}
 	return rep
 }
